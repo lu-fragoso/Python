9abd84673b8490ecc6aacb684607c4b31f70f898
Lucas Fragoso <88888171+lu-fragoso@users.noreply.github.com>
2023-08-21 18:05:40 -0300

    Python treiniment

---
+++ recebe_dados.py
@@ -0,0 +1,16 @@
+#print ('dados: ')
+#dados = input()
+#print (f'dados: {dados}')
+
+dados = input('dado: ')
+print(f'dados: {dados}')
+
+# utilizando input para receber dados
+#realizando o metodo para formatar os dados recebidos
+
+numeros = input()
+print(f'numeros-1: {int(numeros)-1}')
+#necessario tipar a variavel em uma expressao matematica
+
+
+
