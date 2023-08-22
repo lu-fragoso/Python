41509fff3f9302f2e6d942b098ef5ba0d0c7c995
Lucas Garcia Fragoso <fragolca@otg.sor.com.br>
2023-08-22 16:04:56 -0300

    Tipes of Fors

---
+++ for.py
@@ -0,0 +1,41 @@
+nome = "Lucas"
+lista = [1,3,5,7]
+numeros = range(1,20)
+
+"""
+for letra in nome:
+    print(letra)
+
+print('------')
+
+for num in lista:
+    print (num)
+
+print('------') 
+
+for num in numeros:
+    print(num)
+
+#enumerate realiza a numeração de determinado objeto, podendo utilizar o indice ou o dado do objeto
+for valor in enumerate(nome):
+    print(valor[1])
+
+qtd = int(input('Quantas vezes o loop deve rodar? '))
+soma = 0
+
+for n in range(1,qtd+1):
+    num = int(input(f'Infomr o {n}/{qtd} valor: '))
+    soma = soma + num
+    print(f'Imprimindo {n}\n')
+print(f'Soma: {soma}')
+
+for letra in nome:
+    print(letra, end='')
+"""
+
+
+
+
+
+
+
