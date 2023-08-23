3bbd210cc8be9d02b7f6effa27de4c790ce2c7da
Lucas Garcia Fragoso <fragolca@otg.sor.com.br>
2023-08-23 14:15:58 -0300

    Lists, arrays and matrix

---
+++ list.py
@@ -0,0 +1,27 @@
+# listas, arrays ou matrizes -> Todos voltados para agrupar dados
+
+lista1 = [1,3,5,7,9,11,13]
+lista2 = ['a','b','c','d','e','f']
+lista3 = []
+lista4 = list(range(11))
+lista5 = list('OTG')
+
+num = 1 
+
+if num in lista4:
+    print('Achou')
+else:
+    print("Não achou")
+
+
+
+
+
+
+
+
+
+
+
+
+
