e3e464dfb543aaa806b2e6814f366f80f63767b6
Lucas Garcia Fragoso <fragolca@otg.sor.com.br>
2023-08-23 11:13:25 -0300

    Testing range

---
+++ range.py
@@ -0,0 +1,14 @@
+"""for num in range(11):
+    print(num)
+
+for num in range(1,11):
+    print(num)
+
+for num in range(1,10,2):
+    print(num)
+
+for num in range(10,1,-2):
+    print(num)
+    
+    """
+
